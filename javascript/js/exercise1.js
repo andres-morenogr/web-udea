92eb0d04d131a426164e647e8170456468d2a826
Andres Moreno <andresmore06@gmail.com>
2020-09-23 23:49:57 -0500

    Added exercise1 on javascript activity

---
+++ javascript/js/exercise1.js
@@ -0,0 +1,21 @@
+function isPanagram() {
+  const panagram = "abcdefghijklmnopqrstuvwxyz"
+  const textToVerify = document.getElementById("validate").value;
+  let result = document.getElementById("result");
+  let processedText = processText(textToVerify);
+  if(panagram.localeCompare(processedText) === 0) {
+    result.innerHTML = "TRUE"
+  } else {
+    result.innerHTML = "FALSE"
+  }
+}
+
+function processText(text) {
+  text = text.toLowerCase().split(' ').join('');
+  text = sortAlphabetically(text);
+  return [... new Set(text)].join('');
+}
+
+function sortAlphabetically(text) {
+  return [...text].sort();
+}
